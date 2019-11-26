9b65e7ec3c63a65b3fd06a7897cab19f56783144
jpca999 <jpca999@gmail.com>
2019-11-26 00:26:47 -0800

    --

--- src/js/another-file.js
+++ src/js/another-file.js
@@ -1,1 +1,9 @@
+function initialize() {
+    var input = document.getElementById('searchTextField');
+    new google.maps.places.Autocomplete(input);
+  }
+  
+google.maps.event.addDomListener(window, 'load', initialize);
+
 console.log('another-file.js file loaded');
+
--- src/js/detects.js
+++ src/js/detects.js
@@ -1,1 +1,8 @@
+function initialize() {
+    var input = document.getElementById('searchTextField');
+    new google.maps.places.Autocomplete(input);
+  }
+  
+google.maps.event.addDomListener(window, 'load', initialize);
+
 console.log('detects.js file loaded');
